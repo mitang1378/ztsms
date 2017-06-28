f9b129db63d007ec67185fa0df0a922f57a8cd58
MRfeng85 <83398365@qq.com>
2017-06-28 10:13:44 +0800

    first commit

---
+++ src/ZtSend.php
@@ -0,0 +1,48 @@
+<?php
+namespace uka\ztsms;
+
+use linslin\yii2\curl;
+
+class ZtSend{
+
+  public $username;
+
+  public $password;
+
+  public function send_sms($mobile,$content,$productid='887362') {
+    $url = "http://www.ztsms.cn/sendNSms.do";
+    $tkey = date('Ymdhis',time());
+    $password = md5(md5($this->password).$tkey);
+    $data = [
+      'username' => $this->username,
+      'password' => $password,
+      'tkey' => $tkey,
+      'mobile' => $mobile,
+      'content' => $content."【优卡科技】",
+      'productid' => $productid,
+      'xh' => ''
+    ];
+    return $this->send($url, $data);
+  }
+
+  public function balance()
+  {
+    $url = "http://www.ztsms.cn/balanceN.do";
+    $tkey = date('Ymdhis',time());
+    $password = md5(md5($this->password).$tkey);
+    $data = [
+      'username' => $this->username,
+      'password' => $this->password,
+      'tkey' => $tkey
+    ];
+    return $this->send($url, $data);
+  }
+
+  public function send($url,$data)
+  {
+    $curl = new curl\Curl();
+    $response = $curl->setPostParams($data)->post($url);
+    return $response;
+  }
+
+}
